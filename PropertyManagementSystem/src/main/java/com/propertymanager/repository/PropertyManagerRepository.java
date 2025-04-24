ed954a32158f504b1fdf0040f2ab30b8e3ef7d7a
rahulvama <rahulvarmaguduru8@gmail.com>
2025-04-25 01:14:13 +0530

    Update PropertyManagerRepository.java

--- PropertyManagementSystem/src/main/java/com/propertymanager/repository/PropertyManagerRepository.java
+++ PropertyManagementSystem/src/main/java/com/propertymanager/repository/PropertyManagerRepository.java
@@ -2,7 +2,7 @@ package com.propertymanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.propertymanager.entity.PropertyManager;
-
+import java.util.Optional;
 public interface PropertyManagerRepository extends JpaRepository<PropertyManager, Long> {
     Optional<PropertyManager> findByName(String name);
 }
