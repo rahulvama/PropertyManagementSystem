dc3c021429d100a4c8cc120613906550464a1e55
rahulvama <rahulvarmaguduru8@gmail.com>
2025-04-25 01:11:34 +0530

    Update PropertyService.java

--- PropertyManagementSystem/src/main/java/com/propertymanager/service/PropertyService.java
+++ PropertyManagementSystem/src/main/java/com/propertymanager/service/PropertyService.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 
+
 public interface PropertyService {
     Property addProperty(PropertyDto dto);
     Property updateProperty(Long id, PropertyUpdateRequest request);
